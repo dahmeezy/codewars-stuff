fd31c84a79608c5bd9c7dceab6678c6d36eb286b
Zainab Yusuf <161367023+dahmeezy@users.noreply.github.com>
2026-02-21 15:25:02 +0100

    Update string-repeat.go

--- 8-kyu/string-repeat/string-repeat.go
+++ 8-kyu/string-repeat/string-repeat.go
@@ -3,11 +3,11 @@ package kata
 ​
 func RepeatStr(rep int, val string) string {
  if rep > 0 {
-   var new_str string
-   for i := 1; i <= rep; i++ {
-     new_str += val
-   }
-   return new_str
+  var new_str string
+  for i:=0 ; i <= rep ; i++{
+   new_str += val
+  }
+  return new_str
  }
   return ""
  }
