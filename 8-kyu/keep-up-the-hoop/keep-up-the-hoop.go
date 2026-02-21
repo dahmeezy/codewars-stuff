c05607b314c6b1aa4c29e5c813b71d42a0e822cd
Zainab Yusuf <161367023+dahmeezy@users.noreply.github.com>
2026-02-21 11:34:17 +0100

    Add solution - CodeHub

---
+++ 8-kyu/keep-up-the-hoop/keep-up-the-hoop.go
@@ -0,0 +1,12 @@
+package kata
+​
+func HoopCount(n int) string {
+  // your code goes here
+  //if the hoops is 10 and above, cheer Alex on.
+  if n>=10{
+    return "Great, now move on to tricks"
+    //if not encourage him
+  }else {
+  return "Keep at it until you get it"
+    }
+}
