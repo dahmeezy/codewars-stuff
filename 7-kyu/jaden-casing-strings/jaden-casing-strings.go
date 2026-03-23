65bee84d8d41079ca4381b1e65f505f6ef6e530c
Zainab Yusuf <161367023+dahmeezy@users.noreply.github.com>
2026-03-23 11:49:44 +0100

    Update solution - CodeHub

--- 7-kyu/jaden-casing-strings/jaden-casing-strings.go
+++ 7-kyu/jaden-casing-strings/jaden-casing-strings.go
@@ -1,33 +1,14 @@
 package kata
 ​
-import (
-"strings")
+import "strings"
 ​
-func ToJadenCase(str string) string {
-  var res strings.Builder
-  if str==""{
-    return " "
-  } else{
-    words:=strings.Fields(str)
-    for index,word:=range words{
-      for i,c :=range word{
-        if i==0 && (c>='a'&&c<='z'){
-          res.WriteRune(rune(int(c)-32))
-          
-        } else if i >0&&(c>='A'&&c<='Z'){
-          res.WriteRune(rune(int(c)+32))
-        }else{
-          res.WriteRune(c)
-        }
-        
-      }
-      if index==len(words)-1{
-        continue
-      }else{
-        res.WriteString(" ")
-      }
-      
+func ToJadenCase(str string) (res string) {
+  for i, c := range str {
+    if i == 0 || str[i-1] == ' ' {
+      res += strings.ToUpper(string(c))
+    }else {
+      res += string(c)
     }
-  }  
-  return res.String() // your code here...
+  }
+  return
 }
