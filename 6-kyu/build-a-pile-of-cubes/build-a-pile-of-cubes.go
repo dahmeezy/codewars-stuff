2251012a932918eab3189f43b468d64ae44b6052
Zainab Yusuf <161367023+dahmeezy@users.noreply.github.com>
2026-04-27 13:05:19 +0100

    Update solution - CodeHub

--- 6-kyu/build-a-pile-of-cubes/build-a-pile-of-cubes.go
+++ 6-kyu/build-a-pile-of-cubes/build-a-pile-of-cubes.go
@@ -1,29 +1,9 @@
 package kata
 ​
-import (
-"math")
-​
 func FindNb(m int) int {
-  // your code
-//   n=(-1 + \sqrt{1+(8*\sqrt{m})})/2 so thats the calculation i'm doing here
-  s := 8 * math.Sqrt(float64(m))
-  n := int((math.Sqrt(s+1) - 1) / 2)
-  var sum int
-  var count int
-//   reverse loop 
-  for i := n; i > 0; i-- {
-//     cube and store
-    sum+=int(math.Pow(float64(i), 3))
-    count++
+  for n := 1 ; m > 0 ; n++ {
+    m -= n*n*n
+    if m == 0 { return n }
+  }
+  return -1
 }
-//   return number of cubes only if the sum equals m
-  if sum == m {
-    return count
-//     if there is no such n we should return -1(if the sum is not equal to m)
-  } else {
-    return -1
-  }
-​
-  }
-  
-​
