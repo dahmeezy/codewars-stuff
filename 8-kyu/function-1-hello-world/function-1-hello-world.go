4c7fdc8615e84d487cade25f7d8464f511955148
Zainab Yusuf <161367023+dahmeezy@users.noreply.github.com>
2026-05-11 10:45:27 +0100

    Add solution - CodeHub

---
+++ 8-kyu/function-1-hello-world/function-1-hello-world.go
@@ -0,0 +1,13 @@
+package kata // do not remove
+​
+// import "fmt"
+​
+func greet()string{
+//   fmt.Println("hello world!")
+  
+  return "hello world!"
+}
+​
+// function declaration {
+// Unlock the power of go
+//}
