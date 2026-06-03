4e0edfac251416ebfc2c538ff58adad511639d40
Zainab Yusuf <161367023+dahmeezy@users.noreply.github.com>
2026-06-03 11:09:39 +0100

    Update solution - CodeHub

--- 7-kyu/is-this-a-triangle/is-this-a-triangle.go
+++ 7-kyu/is-this-a-triangle/is-this-a-triangle.go
@@ -1,14 +1,6 @@
 package kata
 ​
 func IsTriangle(a, b, c int) bool {
-  
-  if a<=0||b<=0||c<=0{
-    return false
-  }
-  
-  if a<b+c&&b<a+c&&c<a+b{
-    return true
-  }
-  return false
-​
+  return a+b > c && b+c > a && a+c > b
 }
+​
